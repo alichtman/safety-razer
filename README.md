b07f82ff3bcf5fb064751534374323f9caae7874
Aaron Lichtman <aaronlichtman@gmail.com>
2019-03-03 23:39:24 -0600

    Add ridiculous hack to track privelege level and root logins

--- README.md
+++ README.md
@@ -2,25 +2,56 @@
 
 Ever accidentally run a command as `root`? 
 
-`safety-razer` helps keep you aware of when you're logged in as `root` by setting every connected Razer device's lighting effect to a deep red. When you exit from `root`, it will set the lighting effect to a calm blue. 
+`safety-razer` helps keep you aware of when you're logged in as `root` by setting every connected Razer device's lighting effect to a deep red. When you run a command as a non-root user, it will set the lighting effect to a calm blue. 
 
 ### Installation
 
-**Note: Requires an OpenRazer supported Linux distribution.**
+**Notes**
 
-First, install `OpenRazer` by following the instructions at the bottom of [this page](https://openrazer.github.io).
++ An `OpenRazer` supported Linux distribution is needed.
++ `zsh` must be installed.
 
-Then, clone this repository and run the script in the background.
+1. First, install `OpenRazer` by following the instructions at the bottom of [this page](https://openrazer.github.io). For Ubuntu/Linux Mint, the setup is:
 
-```bash
-$ git clone https://github.com/alichtman/safety-razer
-$ cd safety-razer
-$ python3 safety-razer.py &
+```zsh
+$ sudo add-apt-repository ppa:openrazer/stable
+$ sudo apt update
+$ sudo apt install openrazer-meta
 ```
 
-To run `safety-razer` automatically when you open a terminal, run:
+2. Create a new group called `safety-razer-root` for only you and the new `safety-razer` user we're about to create. Create the new `safety-razer` user, add it to the `sudo` and `safety-razer-root` groups, and set `zsh` as the default shell. Then add your user to the `safety-razer-root` group. *(The reason we're doing this is because it's unsafe to change the default login shell on the `root` account.)*
 
-```bash
-$ echo "python3 safety-razer.py &" >> ~/.zshrc
+```zsh
+$ sudo groupadd safety-razer-root
+$ sudo useradd -s `which zsh` -G sudo,safety-razer-root safety-razer
+$ passwd safety-razer # You'll want to password protect this user.
+$ sudo usermod -aG safety-razer-root `whoami`
+```
+
+3. Set the default shell to zsh, and then log out and log back into your user account.
+
+```zsh
+$ chsh -s /bin/bash
+$ logout
+```
+
+4. Next, set the permissions on your user zsh history file so that everyone in the `user-safety-razer` group can read and write to it. Then, set the zsh history file environment variable for the `safety-razer` user to your user's zsh history file path. Then alias `$ su -`.
+
+```zsh
+$ echo $HISTFILE 
+$ sudo chgrp safety-razer-root `$HISTFILE`
+$ sudo chmod g+wr `$HISTFILE`
+$ sudo su safety-razer
+$ sudo -H -u safety-razer zsh -c 'export $HISTFILE=<HISTFILE_PATH_THAT_WAS_PRINTED_BEFORE>'
+$ echo "alias safety-su"="sudo -i safety-razer" >> ~/.zshrc
+```
+
+5. Clone this repository, install the dependencies, and add the script to your `~/.zshrc` to be run on login.
+
+```zsh
+$ git clone https://github.com/alichtman/safety-razer
+$ cd safety-razer
+$ pip3 install -r requirements.txt
+$ echo "sudo python3 safety-razer.py &" >> ~/.zshrc
 ```
 
--- safety-razer.py
+++ safety-razer.py
@@ -1,7 +1,8 @@
 from openrazer.client import DeviceManager
 from colorama import Fore, Style
+from collections import deque
 from loguru import logger
-import subprocess as sp
+from pathlib import Path
 import platform
 import time
 import sys
@@ -11,19 +12,27 @@ import os
 # Globals
 ###
 
+# Colors in RGB
 colors = {
-    "red"  : (255, 0, 0),   #ff0000
-    "blue" : (0, 153, 255)  #0099ff
+	"red"  : (255, 0, 0),
+	"blue" : (0, 153, 255)
 }
 
-STATIC_EFFECT = "static"
+action = {
+	"none": None,
+	"elevate": 1,
+	"de-esecalate": 2
+}
+
 DEBUG = True
-LOG_PATH = "~/.safety-razer.log"
+STATIC_EFFECT = "static"
+LOG_PATH = "{}/.safety-razer.log".format(str(Path.home()))
+
 
 logger.add(LOG_PATH,
-           format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-           backtrace=True,
-           colorize=True)
+		   format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+		   backtrace=True,
+		   colorize=True)
 
 ###
 # Functions
@@ -31,112 +40,140 @@ logger.add(LOG_PATH,
 
 
 def print_status(text):
-    print(Fore.BLUE + Style.BRIGHT + text + Style.RESET_ALL)
+	print(Fore.BLUE + Style.BRIGHT + text + Style.RESET_ALL)
 
 
 def list_razer_devices(device_manager):
-    """
-    List detected Razer devices.
-    """
-    logger.debug("Found {} Razer devices\n".format(len(device_manager.devices)))
-    for device in device_manager.devices:
-        print_status("{}:".format(device.name))
-        print_status("   Type: {}".format(device.type))
-        print_status("   Serial: {}".format(device.serial))
-        print_status("   Firmware version: {}".format(device.firmware_version))
-        print_status("   Driver version: {}".format(device.driver_version))
-        print_status("   Supports static lighting effect: {}\n".format(device.fx.has(STATIC_EFFECT)))
+	"""
+	List detected Razer devices.
+	"""
+	logger.debug("Found {} Razer devices\n".format(len(device_manager.devices)))
+	for device in device_manager.devices:
+		print_status("{}:".format(device.name))
+		print_status("   Type: {}".format(device.type))
+		print_status("   Serial: {}".format(device.serial))
+		print_status("   Firmware version: {}".format(device.firmware_version))
+		print_status("   Driver version: {}".format(device.driver_version))
+		print_status("   Supports static lighting effect: {}\n".format(device.fx.has(STATIC_EFFECT)))
 
 
 def validate_devices(device_list):
-    """
-    Remove all devices from list that don't have the static effect.
-    """
-    for device in device_list:
-        if not device.fx.has(STATIC_EFFECT):
-            logger.debug("Skipping device " + device.name + " (" + device.serial + "). Static not detected.")
-            device_list.remove(device)
+	"""
+	Remove all devices from list that don't have the static effect.
+	"""
+	for device in device_list:
+		if not device.fx.has(STATIC_EFFECT):
+			logger.debug("Skipping device " + device.name + " (" + device.serial + "). Static not detected.")
+			device_list.remove(device)
 
-    return device_list
+	return device_list
 
 
 def compatibility_check():
-    """
-    If this is not being run on a Linux machine, exit.
-    """
-    if platform.system() != "Linux":
-        logger.exception("Linux not detected.")
-        sys.exit(1)
+	"""
+	If this is not being run on a Linux machine, exit.
+	"""
+	if platform.system() != "Linux":
+		logger.exception("Linux not detected.")
+		sys.exit(1)
 
 
 def create_logfile():
-    """
-    Make sure the LOG_PATH is a file. Create it if it doesn't exist.
-    """
-    if os.path.isfile(LOG_PATH):
-        pass
-    elif os.path.isdir(LOG_PATH):
-        # This will be printed to stdout, too
-        logger.exception("LOG_PATH is set to a directory: {}.".format(LOG_PATH))
-        sys.exit()
-    else:
-        os.mknod(LOG_PATH)
-        logger.debug("Created log file: {}.".format(LOG_PATH))
+	"""
+	Make sure the LOG_PATH is a file. Create it if it doesn't exist.
+	"""
+	if os.path.isfile(LOG_PATH):
+		pass
+	elif os.path.isdir(LOG_PATH):
+		# This will be printed to stdout, too
+		logger.exception("LOG_PATH is set to a directory: {}.".format(LOG_PATH))
+		sys.exit()
+	else:
+		os.mknod(LOG_PATH)
+		logger.debug("Created log file: {}.".format(LOG_PATH))
 
 
-def detect_root():
-    """
-    Determine if a user is logged in as root.
-    :return: True if the user is logged in, False otherwise
-    :rtype: Boolean
-    """
-    if "root" == sp.check_output(['whoami']).strip():
-        logger.debug("Detected root.")
-        return True
-    else:
-        logger.debug("Detected user.")
-        return False
+def get_history_file_path():
+	return os.environ(["HISTFILE"])
+
+
+def get_file_len(history):
+	with open(history, "r") as f:
+		return len(f.readlines())
+
+
+def get_unprocessed_commands(history, last_index):
+	with open(history, "r") as f:
+		all_lines = f.readlines()
+		return [line.strip() for line in all_lines][:last_index], len(all_lines)
+
+
+def process_commands(commands, status_stack):
+	escalate = ["sudo", "su"]
+	de_secalate = ["exit"]
+	privelege_change_commands = [command for command in commands
+								if command.startswith(any(escalate + de_secalate))]
+
+	logger.debug("Found {} privelege change commands".format(len(privelege_change_commands)))
+	logger.debug(privelege_change_commands)
+
+	orig_len = len(status_stack)
+
+	if len(privelege_change_commands) == 0:
+		return action["none"], status_stack
+	else:
+		for x in privelege_change_commands:
+			for item in escalate:
+				if x.startswith(item):
+					status_stack.append("root")
+			for item in de_secalate:
+				if x.startswith(item):
+					status_stack.pop()
+
+	if orig_len > len(status_stack):
+		return action["elevate"], status_stack
+	else:
+		return action["de-esecalate"], status_stack
 
 
 def main():
-    """
-    Check if a user is root. If they are, set the keyboard to bright red.
-    Otherwise, set the keyboard to bright blue.
-    """
-    logger.debug("Opening safety-razer.")
-    compatibility_check()
-    device_manager = DeviceManager()
-    if DEBUG:
-        list_razer_devices(device_manager)
+	"""
+	Check if a user is root. If they are, set the keyboard to bright red.
+	Otherwise, set the keyboard to bright blue.
+	"""
+	logger.debug("Opening safety-razer.")
+	compatibility_check()
+	device_manager = DeviceManager()
+	if DEBUG:
+		list_razer_devices(device_manager)
 
-    already_detected_root = False
-    already_detected_user = False
+	hist_file = get_history_file_path()
 
-    # Check to update lighting every 5 seconds
-    while True:
-        for device in validate_devices(device_manager.devices):
-            if detect_root():
-                already_detected_user = False
-                if already_detected_root:
-                    print_status("Still root. No color change needed.")
-                    continue
+	# Track privilege level using a stack
+	tracked_privilege = deque()
+	tracked_privilege.append("user")
 
-                already_detected_root = True
-                if device.fx.static(*colors["red"]):
-                    logger.debug("Successfully set {} to static red".format(device.name))
+	last_hist_index = get_file_len(hist_file)
 
-            else:
-                if already_detected_user:
-                    print_status("Still user. No color change needed.")
-                    continue
+	# Check every 5 seconds
+	while True:
+		commands, last_hist_index = get_unprocessed_commands(hist_file, last_hist_index)
+		action, tracked_privilege = process_commands(commands, tracked_privilege)
 
-                already_detected_user = True
-                already_detected_root = False
-                if device.fx.static(*colors["blue"]):
-                    logger.debug("Successfully set {} to static blue".format(device.name))
+		if action == action["escalate"]:
+			for device in validate_devices(device_manager.devices):
+				if device.fx.static(*colors["red"]):
+					logger.debug("Successfully set {} to static red".format(device.name))
 
-        time.sleep(5)
+		elif action == action["de-esecalate"]:
+			if device.fx.static(*colors["blue"]):
+				logger.debug("Successfully set {} to static blue".format(device.name))
+
+		else:
+			logger.debug("No privelege change detected.")
+
+		time.sleep(5)
 
 
 if __name__ == '__main__':
-    main()
+	main()
